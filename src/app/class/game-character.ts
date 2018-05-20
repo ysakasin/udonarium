d24a2184e8f15fbbdf3527a44730ece5fc4d33f0
blhsrwznrghfzpr <blhsrwznrghfzpr.hompyhrln@gmail.com>
2018-05-20 15:30:28 +0900

    fix: サンプルキャラ生成時のcurrentValueがnumberになるように

--- src/app/class/game-character.ts
+++ src/app/class/game-character.ts
@@ -46,8 +46,8 @@ export class GameCharacter extends TabletopObject {
     }
 
     let resourceElement: DataElement = DataElement.create('リソース', '', {}, 'リソース' + this.identifier);
-    let hpElement: DataElement = DataElement.create('HP', 200, { 'type': 'numberResource', 'currentValue': '200' }, 'HP_' + this.identifier);
-    let mpElement: DataElement = DataElement.create('MP', 100, { 'type': 'numberResource', 'currentValue': '100' }, 'MP_' + this.identifier);
+    let hpElement: DataElement = DataElement.create('HP', 200, { 'type': 'numberResource', 'currentValue': 200 }, 'HP_' + this.identifier);
+    let mpElement: DataElement = DataElement.create('MP', 100, { 'type': 'numberResource', 'currentValue': 100 }, 'MP_' + this.identifier);
 
     this.commonDataElement.appendChild(nameElement);
     this.commonDataElement.appendChild(sizeElement);
